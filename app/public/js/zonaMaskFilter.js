4eddac76ac966566c5e1588f21138efe0a895995
vitorregisr <vitorregisrr@gmail.com>
2019-03-12 09:20:03 -0300

    selectipo now dont show Todos on edit and new prop

--- app/public/js/zonaMaskFilter.js
+++ app/public/js/zonaMaskFilter.js
@@ -1,7 +1,7 @@
 if (document.querySelector('input[name="selectedTipo"]')) {
     const tipoSelecionado = document.querySelector('input[name="selectedTipo"]').value,
         tiposUrbanos = `<optgroup label="Tipos Urbanos" id="selectUrbano">
-                            <option value="">Todos</option>
+                            ${ $('input[name="selectedTipo"]').attr('data-disableTipo') ? '<option value="">Todos</option>' : '' }
                             <option ${"Casa"==tipoSelecionado?"selected":""} value="Casa">Casa</option>
                             <option ${"Apartamento"==tipoSelecionado?"selected":""} value="Apartamento">Apartamento</option>
                             <option ${"Kitnet"==tipoSelecionado?"selected":""} value="Kitnet">Kitnet</option>   
@@ -9,21 +9,33 @@ if (document.querySelector('input[name="selectedTipo"]')) {
                             <option ${"Escritório"==tipoSelecionado?"selected":""} value="Escritório">Escritório</option>
                             <option ${"Terreno"==tipoSelecionado?"selected":""} value="Terreno">Terreno</option>
                         </optgroup>`,
-        tiposRurais = `<optgroup label="Tipos Rurais" id="selectRural">\n          <option value="">Todos</option>      <option ${"Chácara"==tipoSelecionado?"selected":""} value="Chácara">Chácara</option>\n                            <option ${"Sítio"==tipoSelecionado?"selected":""} value="Sítio">Sítio</option>\n                            <option ${"Campo"==tipoSelecionado?"selected":""} value="Campo">Campo</option>\n                            <option ${"Terreno"==tipoSelecionado?"selected":""} value="Terreno">Terreno</option>\n                            <option ${"Lavoura"==tipoSelecionado?"selected":""} value="Lavoura">Lavoura</option>\n                        </optgroup>`,
-        tiposVazio = '<option disabled selected  label="Zona não selecionada"> Tipo de imóvel </option>\n                            <optgroup label="Zona não selecionada">\n                                <option></option>\n                    </optgroup>',
-        setForm = () => {
-            switch ($("#inputFiltrando") && $("#inputFiltrando").val($("#selectZona").val()), $("#selectZona").val()) {
-                case "todos":
-                    $("#selectTipo").html(tiposVazio);
-                    break;
-                case "Urbana":
-                    $("#selectTipo").html(tiposUrbanos), $(".urbano-form").removeClass("hidden"), $(".urbano-form").addClass("visible"), $(".rural-form").removeClass("visible"), $(".rural-form").addClass("hidden");
-                    break;
-                case "Rural":
-                    $("#selectTipo").html(tiposRurais), $(".rural-form").removeClass("hidden"), $(".rural-form").addClass("visible"), $(".urbano-form").removeClass("visible"), $(".urbano-form").addClass("hidden")
-            }
-            $(".selectpicker").selectpicker("refresh")
-        };
+
+        tiposRurais = `<optgroup label="Tipos Rurais" id="selectRural">
+                            ${ $('input[name="selectedTipo"]').attr('data-disableTipo') ? '<option value="">Todos</option>' : '' }
+                            <option ${"Chácara"==tipoSelecionado?"selected":""} value="Chácara">Chácara</option>
+                            <option ${"Sítio"==tipoSelecionado?"selected":""} value="Sítio">Sítio</option>  
+                            <option ${"Campo"==tipoSelecionado?"selected":""} value="Campo">Campo</option>
+                            <option ${"Terreno"==tipoSelecionado?"selected":""} value="Terreno">Terreno</option>
+                            <option ${"Lavoura"==tipoSelecionado?"selected":""} value="Lavoura">Lavoura</option>
+                        </optgroup>`,
+
+        tiposVazio = `<option disabled selected  label="Zona não selecionada"> Tipo de imóvel </option>
+                        <optgroup label="Zona não selecionada">
+                        <option></option>
+                      </optgroup>`
+    setForm = () => {
+        switch ($("#inputFiltrando") && $("#inputFiltrando").val($("#selectZona").val()), $("#selectZona").val()) {
+            case "todos":
+                $("#selectTipo").html(tiposVazio);
+                break;
+            case "Urbana":
+                $("#selectTipo").html(tiposUrbanos), $(".urbano-form").removeClass("hidden"), $(".urbano-form").addClass("visible"), $(".rural-form").removeClass("visible"), $(".rural-form").addClass("hidden");
+                break;
+            case "Rural":
+                $("#selectTipo").html(tiposRurais), $(".rural-form").removeClass("hidden"), $(".rural-form").addClass("visible"), $(".urbano-form").removeClass("visible"), $(".urbano-form").addClass("hidden")
+        }
+        $(".selectpicker").selectpicker("refresh")
+    };
     setForm(), $("#selectZona").change(setForm)
 }
 $("#selectMunicipio").change(function () {
